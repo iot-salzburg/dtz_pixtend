155bdac392d11ddffddce033b776c079195ac705
aniederm <41986362+aniederm@users.noreply.github.com>
2018-08-13 09:17:54 +0200

    Update manual_control_x.py

--- manual_control_x.py
+++ manual_control_x.py
@@ -1,11 +1,12 @@
+#!/usr/bin/env python
 # Salzburg Research ForschungsgesmbH
 # Christoph Schranz & Armin Niedermueller
 
 # Conveyor Belt Control - PiXtend
 # The electronic layout is from the tutorial:
 # https://www.rototron.info/raspberry-pi-stepper-motor-tutorial/
- 
-from ConveyorBelt import ConveyorBelt
 
-with ConveyorBelt as conbelt:
+from ConveyorBeltX import ConveyorBeltXResource
+
+with ConveyorBeltXResource() as conbelt:
     conbelt.manual_control()
