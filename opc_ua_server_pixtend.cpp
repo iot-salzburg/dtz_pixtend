9f90bf3684abf2ff9c0407565db2bfc542e46593
aniederm <armin.niedermueller@salzburgresearch.at>
2018-08-21 10:49:26 +0200

    first try of working server.cpp

--- opc_ua_server_pixtend.cpp
+++ opc_ua_server_pixtend.cpp
@@ -4,8 +4,10 @@
 // OPC UA Server on PiXtend
 
 #include <iostream>
+#include <fstream>
 #include <algorithm>
 #include <time.h>
+#include <string>
 
 #include <thread>
 #include <chrono>
@@ -13,7 +15,6 @@
 #include <opc/ua/node.h>
 #include <opc/ua/subscription.h>
 #include <opc/ua/server/server.h>
-
 
 
 
@@ -37,30 +38,31 @@ std::vector<OpcUa::Variant> MyMethod(NodeId context, std::vector<OpcUa::Variant>
 
 void RunServer()
 {
-  //First setup our server
+  // First setup our server
   auto logger = spdlog::stderr_color_mt("server");
   OpcUa::UaServer server(logger);
   server.SetEndpoint("opc.tcp://localhost:4840/freeopcua/server");
   server.SetServerURI("urn://exampleserver.freeopcua.github.io");
   server.Start();
 
-  //then register our server namespace and get its index in server
-  uint32_t idx = server.RegisterNamespace("http://examples.freeopcua.github.io");
+  // then register our server namespace and get its index in server
+  uint32_t idx = server.RegisterNamespace("Namespace - DTZ PiXtend");
 
-  //Create our address space using different methods
+  // Create our address space using different methods
   Node objects = server.GetObjectsNode();
 
-  //Add a custom object with specific nodeid
-  NodeId nid(99, idx);
-  QualifiedName qn("NewObject", idx);
-  Node newobject = objects.AddObject(nid, qn);
+  // Add a Node named "Parameters" with the id "1" and in namespace idx (index)
+  NodeId nid(1, idx);
+  QualifiedName qn("Parameters", idx);
+  Node param_object = objects.AddObject(nid, qn);  
+    
+  // Add a variable and a property with auto-generated nodeid to our custom object
+  Node conbelt_state = param_object.AddVariable(idx, "Conveyorbelt - State", Variant(8));
+  Node conbelt_distance = param_object.AddVariable(idx, "Conveyorbelt - Distance", Variant(8.8));
+  Node conbelt_drive = param_object.AddMethod(idx, "Conveyorbelt - Drive Control", MyMethod);
 
-  //Add a variable and a property with auto-generated nodeid to our custom object
-  Node myvar = newobject.AddVariable(idx, "MyVariable", Variant(8));
-  Node myprop = newobject.AddVariable(idx, "MyProperty", Variant(8.8));
-  Node mymethod = newobject.AddMethod(idx, "MyMethod", MyMethod);
 
-  //browse root node on server side
+  // browse root node on server side
   Node root = server.GetRootNode();
   logger->info("Root node is: {}", root);
   logger->info("Children are:");
@@ -71,42 +73,65 @@ void RunServer()
     }
 
 
-  //Uncomment following to subscribe to datachange events inside server
+  // Uncomment following to subscribe to datachange events inside server
   /*
   SubClient clt;
   std::unique_ptr<Subscription> sub = server.CreateSubscription(100, clt);
-  sub->SubscribeDataChange(myvar);
+  sub->SubscribeDataChange(conbelt_state);
   */
 
 
 
-  //Now write values to address space and send events so clients can have some fun
-  uint32_t counter = 0;
-  myvar.SetValue(Variant(counter)); //will change value and trigger datachange event
+  // Now write values to address space and send events so clients can have some fun
+  conbelt_state.SetValue(Variant("init")); //will change value and trigger datachange event
+  conbelt_distance.SetValue(Variant(0.0)); 
 
-  //Create event
+  // Create event
   server.EnableEventNotification();
-  Event ev(ObjectId::BaseEventType); //you should create your own type
+  Event ev(ObjectId::BaseEventType); // you should create your own type
   ev.Severity = 2;
   ev.SourceNode = ObjectId::Server;
   ev.SourceName = "Event from FreeOpcUA";
   ev.Time = DateTime::Current();
 
+  // define filestreams
+  std::ifstream statefile("state.log");
+  std::ifstream distancefile ("distance.log");
+  std::string line;
 
   logger->info("Ctrl-C to exit");
 
   for (;;)
     {
-      myvar.SetValue(Variant(++counter)); //will change value and trigger datachange event
+
+	  // read conbelt_state from file and set value inside our object
+	  getline(statefile, line);
+      conbelt_state.SetValue(Variant(line));		// will change value and trigger datachange event
+
+
+	  // read conbelt_distance from file and set value inside our object
+	  getline(distancefile, line);
+	  std::string::size_type sz;			 
+	  double distance = std::stod(line, &sz);	
+	  conbelt_distance.SetValue(Variant(distance)); // will change value and trigger datachange event
+
+
       std::stringstream ss;
-      ss << "This is event number: " << counter;
+	  ss << "This is an conbelt_state change event";
       ev.Message = LocalizedText(ss.str());
       server.TriggerEvent(ev);
       std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+
+
     }
 
+  // close filestreams and shut down server
+  statefile.close();
+  distancefile.close();
   server.Stop();
 }
+
+// MAIN
 
 int main(int argc, char ** argv)
 {
