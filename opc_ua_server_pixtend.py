5c4df925a75d5619ad8732830420e919ef04b3d4
aniederm <41986362+aniederm@users.noreply.github.com>
2018-08-29 13:42:56 +0200

    fully working

--- opc_ua_server_pixtend.py
+++ opc_ua_server_pixtend.py
@@ -10,27 +10,25 @@ import time
 
 from ConveyorBeltX import ConveyorBeltX
 from opcua import ua, uamethod, Server
-from multiprocessing import Process
 import datetime
 import time
+#import threading
 
-def move_belt_core(drive):
-    print("own process")
+conbelt = ConveyorBeltX()
 
-    conbelt = ConveyorBeltX()
-
-    if drive:
-        conbelt.move_right_for(0.55) # 55cm
-    else:
-        conbelt.move_left_for(0.55)  # 55cm
-    conbelt.halt()
+def move_belt_core(direction, distance):
+    if direction == "right":
+        conbelt.move_right_for(distance)
+    elif direction == "left":
+        conbelt.move_left_for(distance)
     return True
 
 @uamethod
-def move_belt(parent, drive):
+def move_belt(parent, direction, distance):
     print("move_belt function")
-    process = Process(target=move_belt_core, args=(drive,))
-    process.start()
+    move_thread = threading.Thread(name='move_belt_thread', target = move_belt_core, args = (direction, distance,)) 
+    move_thread.daemon = True
+    move_thread.start()
     return True
 
 if __name__ == "__main__":
@@ -49,41 +47,43 @@ if __name__ == "__main__":
     objects = server.get_objects_node()
 
     # Add a parameter object to the address space
-    object_1 = objects.add_object(idx, "Object1")
+    object1 = objects.add_object(idx, "Object1")
 
     # Parameters - Addresspsace, Name, Initial Value
-    Time = object_1.add_variable(idx, "Time", 0)
-    mover = object_1.add_method(idx, "MoveBelt", move_belt, [ua.VariantType.Boolean], [ua.VariantType.Boolean])
-    ConBeltState = object_1.add_variable(idx, "Conveyor Belt - State", "init")
-    ConBeltDistance = object_1.add_variable(idx, "Conveyor Belt - Distance", 0.0)
+    server_time = object1.add_variable(idx, "ServerTime", 0)
+    mover = object1.add_method(idx, "MoveBelt", move_belt, [ua.VariantType.String, ua.VariantType.Float], [ua.VariantType.Boolean])
+    conbelt_state = object1.add_variable(idx, "ConBeltState", "init")
+    conbelt_dist = object1.add_variable(idx, "ConBeltDist", 0.0)
 
     # Set parameters writable by clients
-    Time.set_writable()
+    server_time.set_writable()
 
 
     # Start the server
     server.start()
     print("OPCUA - Pixtend - Server started at {}".format(url))
 
-try:
-    # Assign random values to the parameters
-    while True:
-        TIME = datetime.datetime.now()  # current time
-        with open("state.log") as f:
-            state = f.read()
-        with open("distance.log") as f:
-            distance = f.read()
+    try:
+        # Assign random values to the parameters
+        while True:
+            TIME = datetime.datetime.now()  # current time
+            with open("state.log") as f:
+                state = f.read()
+            with open("distance.log") as f:
+                distance = f.read()
 
-        # set the random values inside the node
-        print(state, distance, Time)
-        Time.set_value(TIME)
-        ConBeltState.set_value(state)
-        ConBeltDistance.set_value(distance)
+            # set the random values inside the node
+            print("Belt-State: " + str(state) + "   Belt-Distance: " + str(distance) + "   Server-Time: " + str(server_time.get_value()))
+            server_time.set_value(TIME)
+            conbelt_state.set_value(state)
+            conbelt_dist.set_value(distance)
 
-        # sleep 2 seconds
-        time.sleep(2)
-except KeyboardInterrupt:
-        print("\nCtrl-C pressed. OPCUA - Pixtend - Server stopped at {}".format(url))
-finally:
-    #close connection, remove subcsriptions, etc
-    server.stop()
+           # sleep 2 seconds
+            time.sleep(2)
+    except KeyboardInterrupt:
+            print("\nCtrl-C pressed. OPCUA - Pixtend - Server stopped at {}".format(url))
+    finally:
+        #close connection, remove subcsriptions, etc
+        server.stop()
+        conbelt = None
+        sys.exit(0)
